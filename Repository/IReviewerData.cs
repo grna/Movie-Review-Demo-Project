fe1a71630d2df5324614e1d873812a22ef472d26
grna <31864339+grna@users.noreply.github.com>
2018-11-22 16:01:05 +0200

    Update and rename Services/IReviewerData.cs to Repository/IReviewerData.cs

--- Repository/IReviewerData.cs
+++ Repository/IReviewerData.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace SciFiReviewsApi.Services
+namespace SciFiReviewsApi.Repository
 {
     public interface IReviewerData
     {
