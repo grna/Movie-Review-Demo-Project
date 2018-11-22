52b0bb467a1fca67f843bd31c3227855b820be57
grna <31864339+grna@users.noreply.github.com>
2018-11-22 16:02:32 +0200

    Update and rename Services/SqlMovieData.cs to Repository/SqlMovieData.cs

--- Repository/SqlMovieData.cs
+++ Repository/SqlMovieData.cs
@@ -9,7 +9,7 @@ using SciFiReviewsApi.Models.DtoModels;
 using SciFiReviewsApi.Models.EntityModels;
 using SciFiReviewsApi.Models.ReturnModels;
 
-namespace SciFiReviewsApi.Services
+namespace SciFiReviewsApi.Repository
 {
     public class SqlMovieData : IMovieData
     {
